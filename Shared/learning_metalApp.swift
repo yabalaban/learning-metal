168293ffcb8350443fe1283ab63823092c0703e0
Alexander Balaban <balaban.alexander+github@gmail.com>
2022-09-28 02:15:29 +0100

    Initial Commit

---
+++ Shared/learning_metalApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_metalApp.swift
+//  Shared
+//
+//  Created by Alexander Balaban on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct learning_metalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
